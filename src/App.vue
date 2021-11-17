e0ad0f9361199b93c15655402d671da684d84f76
MAERZzz <73137287+MAERZzz@users.noreply.github.com>
2021-11-17 13:36:42 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <v-app>
   <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            КИПУ
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Учебный проект
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
+    <v-list-item>
+      <v-list-item-content>
+            <v-list-item-title class="title">
+              КИПУ
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              Учебный проект
+            </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+    <v-list dense>
         <v-list-item-group color="primary">
-          <v-list-item 
-          v-for="link in links" 
+          <v-list-item
+          v-for="link in links"
           :key="link.title"
           :to="link.url"
           >
@@ -29,21 +27,21 @@
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
-      </v-list>
+    </v-list>
   </v-navigation-drawer> 
   <v-app-bar app dark color="primary">
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
        <v-btn 
-       v-for="link in links" 
-       :key="link.title" 
+       v-for="link in links"
+       :key="link.title"
        :to="link.url" 
-       text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn> 
+       text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>
     </v-toolbar-items>
   </v-app-bar>
   <v-main>
-    <!--<router-view></router-view>--> 
+    <router-view></router-view> 
   </v-main>
   </v-app>
 </template>
@@ -53,14 +51,15 @@ export default {
   data() {
     return {
       drawer: false,
-      links: [
+      links:[
       {title:"Login", icon:"mdi-lock", url:"/login"},
-      {title:"Registration", icon:"mdi-face-man", url:"/registration"},
-      {title:"Orders", icon:"mdi-bookmark-multiple-outline", url:"/orders"},
+      {title:"Registration",icon:"mdi-face-man",url:"/registration"},
+      {title:"Orders",icon:"mdi-bookmark-multiple-outline",
+      url:"/orders"},
       {title:"New ad", icon:"mdi-note-plus-outline", url:"/new"},
       {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
       ]
     }
   }
-}
+};
 </script>
