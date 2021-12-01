6d2b64bcef54f65c530b8bcf96160a5362b2e999
MAERZzz <ernest.mamutov.03@mail.ru>
2021-12-01 15:05:22 +0300

    Firebase: подключение и аутентификация

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
+//import fb from 'firebase'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -12,5 +15,19 @@ new Vue({
   vuetify,
   render: h => h(App),
   router:router,
-  store
+  store,
+  created(){
+  const firebaseConfig = {
+  apiKey: "AIzaSyAMZX7lF4GMpt8xYfdm3RHw8EpIOJDqbvA",
+  authDomain: "ad-project-5fcc8.firebaseapp.com",
+  projectId: "ad-project-5fcc8",
+  storageBucket: "ad-project-5fcc8.appspot.com",
+  messagingSenderId: "1055263872410",
+  appId: "1:1055263872410:web:9bff162d8c6baeeec95623"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+  getAnalytics(app);
+}
 }).$mount('#app')
