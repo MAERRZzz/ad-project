4647b1dec1e563156eba5f4ec23e5311762c662f
MAERZzz <ernest.mamutov.03@mail.ru>
2021-12-01 12:34:17 +0300

    10.5 детали объявления

--- src/store/ads.js
+++ src/store/ads.js
@@ -1,36 +1,35 @@
 export default {
 	state: {
 		ads:[
-			{
-				title:"First",
-				desc:"First Desc",
-				promo: true,
-				src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-				id:"1"
-			},
-			{
-				title:"Second",
-				desc:"Second Desc",
-				promo: true,
-				src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-				id:"2"
-			},
-			{
-				title:"Third",
-				desc:"Thitd Desc",
-				promo: true,
-				src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-				id:"3"
-			},
-			{
-				title:"Fouth",
-				desc:"Fouth Desc",
-				promo: true,
-				src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-				id:"4"
-			}
-	]
-},
+                {
+                    title:"First",
+                    desc:"First Desc",
+                    promo: true,
+                    src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+                    id:"1"
+                },
+                {
+                    title:"Second",
+                    desc:"Second Desc",
+                    promo: true,
+                    src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+                    id:"2"
+                },
+                {
+                    title:"Third",
+                    desc:"Thitd Desc",
+                    promo: true,
+                    src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+                    id:"3"
+                },
+                {
+                    title:"Fouth",
+                    desc:"Fouth Desc",
+                    promo: true,
+                    src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+                    id:"4"
+                }
+            ]},
 	mutations: {},
 	actions: {},
 	getters: {
@@ -44,6 +43,11 @@ export default {
 		},
 		myAds(state) {
 			return state.ads
-	}
+        },
+        adById(state) {
+            return id => {
+            return state.ads.find(ad => ad.id == id)
+            }
+        }        
 }
 }
