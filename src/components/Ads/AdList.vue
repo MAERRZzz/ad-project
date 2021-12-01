b3dbd7a36b97eec1d7e475bc036175a79f64fd0a
MAERZzz <ernest.mamutov.03@mail.ru>
2021-12-01 10:55:49 +0300

    8.1 верстка списка

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,10 +1,34 @@
 <template> 
 	<v-container>
 		<v-layout row> 
-			<v-flex xs12>
-				<h1>Ad list</h1> 
+			<v-flex xs12 sm6 offset-sm3>
+				<h1 class="text--secondary mb-3 mt-3">My ads</h1>
+				<v-card class="elevation-10 mb-5" v-for="ad in ads" :key="ad.id">
+					<v-layout row>
+						<v-flex xs4 class="pl-3">
+							<v-img
+								:src="ad.src"
+								height="175px">
+							</v-img>	
+						</v-flex>
+						<v-flex xs8 class="pr-3">
+						<v-card-text>
+							<h2 class="text--primary">{{ad.title}}</h2>
+							<p>{{ad.desc}}</p>
+						</v-card-text>
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn 
+								color="primary"
+								:to="'/ad/' + ad.id">
+								Open
+							</v-btn>
+						</v-card-actions>	
+						</v-flex>
+					</v-layout>
+				</v-card>
 			</v-flex>
-		</v-layout> 
+		</v-layout>
 	</v-container>
 </template>
 <script>
